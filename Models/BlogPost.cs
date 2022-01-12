9d6409c65c408bce07f494dd96cd85cbe89a86e9
MikeOliwa <mikeyy111@googlemail.com>
2022-01-12 13:26:41 +0100

    Add project files.

---
+++ Models/BlogPost.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogBase.Models {
+    public class BlogPost {
+
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public int MyProperty { get; set; }
+
+    }
+}
